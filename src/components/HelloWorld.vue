fa2a1b2c0b0d4063f8a6eb5453ca361c475ac610
NakanoNamiki <namiki.nakano.vertex@gmail.com>
2020-04-13 13:55:15 +0900

    #1 v-if

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="hello">
+  <div class="test">
     <h1>{{ title }}</h1>
     <h2>{{ text }}</h2>
     <h3>{{ todoList }}</h3>
     <h3>{{ doneList }}</h3>
     <h4 v-show="showText">{{ showSampleText }}</h4>
     <button v-on:click="toggle">隠しボタン *絶対に押さないでね！*</button>
+
+    <h1>あなたの会員ランク</h1>
+    <p v-if="isMember && isPremier">「 あなたはプレミアム会員です 」</p>
+    <p v-else-if="isMember && !isPremier">「 あなたは一般会員です 」</p>
+    <p v-else-if="!isMember && !isPremier">「 あなたは会員ではありません 」</p>
+    <p v-else>不正なユーザーです</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "test",
   data() {
     return {
       title: "Vue.js",
       text: "文法テスト編",
-      todoList: "v-if, v-for, v-bind, v-model, computed, watch",
-      doneList: "マスタッシュ記法, data, v-on, methods",
+      todoList: "v-for, v-bind, v-model, computed, watch",
+      doneList: "マスタッシュ記法, data, v-on, methods, v-if, ",
       showText: false,
-      showSampleText: "いやん///"
+      showSampleText: "いやん///",
+      isMember: true,
+      isPremier: true
     };
   },
   methods: {
